550d8663200ccba902043432b321d755d06e7da6
dahe <Dengdaheng67@gmail.com>
2022-01-07 14:02:38 +0700

    a

--- re-learn-react/week2/react-project/jd-admin/src/api/admin.js
+++ re-learn-react/week2/react-project/jd-admin/src/api/admin.js
@@ -3,3 +3,7 @@ import request from './../utils/request'
 export function adminLogin (params) {
    return  request.get('account.json', { params })
 }
+
+export function loadAdminList (params) {
+   return request.get('/adminlist.json', { params })
+}
--- re-learn-react/week2/react-project/jd-admin/src/views/user/AdminList.jsx
+++ re-learn-react/week2/react-project/jd-admin/src/views/user/AdminList.jsx
@@ -1,5 +1,17 @@
-import React, { useState } from 'react';
-import { Table, Button, Drawer, Form, Input, Select, Tree } from 'antd'
+import React, { useState, useEffect } from 'react';
+import { 
+    Table,
+    Button,
+    Drawer,
+    Form,
+    Input,
+    Select,
+    Tree,
+    Space,
+    Tag,
+    Modal
+} from 'antd'
+import { loadAdminList } from './../../api/admin'
 import menus from './../../router/menu'
 
 menus.splice(0, 1) //过滤重定向的第一项
@@ -8,16 +20,49 @@ const treeData = menus
 
 const AdminList = () => {
     const [adminlist, setAdminList] = useState([])
+    useEffect(() => {
+        loadAdminList({}).then(res => {
+            // console.log(res)
+            setAdminList(res.data.result)
+        })
+    }, [])
     const [visible, setVisiable] = useState(false)
-
     const [adminname, setAdminName] = useState('')
     const [password, setPassWord] = useState('')
     const [role, setRole] = useState(1)
     const [checkedKeys, setCheckedKeys] = useState([])
-
-
-    const columns = []
-
+    const [modalVisible, setModalVisible] = useState(false)
+    
+    const columns = [
+        {
+            title: "序号",
+            render:(text, record, index) => <span>{ index }</span>
+        },
+        {
+            title: '管理员名称',
+            dataIndex: 'adminname',
+            key: 'adminname'
+        },
+        {
+            title: '管理员角色',
+            dataIndex: 'roleid',
+            key: 'roleid',
+            render: (text, record, index) => <Tag color="magenta">{ text === 1 ? '普通管理员' : '超级管理员' }</Tag>
+        },
+        {
+            title: "操作",
+            render: (text, record, index) => {
+                return (
+                    <>
+                     <Space>
+                         <Button type="primary" onClick={ () => editAdmin(record) }>编辑</Button>
+                         <Button type="dashed" danger>删除</Button>
+                     </Space>
+                    </>
+                )
+            }
+        }
+    ]
     const addAdmin = () => {
         let params = {
             "adminname": adminname,
@@ -28,8 +73,11 @@ const AdminList = () => {
         console.log(params)
         //在这里调用添加管理员的接口
     }
-   
-
+    const editAdmin = (record) => {
+        setAdminName(record.adminname)
+        setRole(record.roleid)
+        setModalVisible(true)
+    }
     return (
        <>
         <Button type="primary" onClick={ () => setVisiable(true) } style={{ marginBottom: 20 }}>添加管理员</Button>
@@ -94,6 +142,27 @@ const AdminList = () => {
             setCheckedKeys(info.checkedNodes)
         }}/>
       </Drawer>
+        {/* 编辑管理员 */}
+        <Modal title="Basic Modal" 
+        visible={modalVisible} 
+        onOk={() => {
+            setModalVisible(false)
+        }} 
+        onCancel={() => {
+            setModalVisible(false)
+        }}>
+        <Form>
+            <Form.Item>
+                <Input value={ adminname }  placeholder="请输入管理员账号" onChange={ (e) => setAdminName(e.target.value) }></Input>
+            </Form.Item>
+            <Form.Item>
+                <Select value={ role } onChange={ (value) => setRole(value) }>
+                    <Select.Option value={ 2 }>超级管理员</Select.Option>
+                    <Select.Option value={ 1 }>管理员</Select.Option>
+                </Select>
+            </Form.Item>
+        </Form>
+      </Modal>
        </>
     );
 }

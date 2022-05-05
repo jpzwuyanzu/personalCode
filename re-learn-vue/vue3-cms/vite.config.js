16842fb3dd06f4210350fd2eb4bb5618c24a27e9
dahe <Dengdaheng67@gmail.com>
2022-05-05 16:26:47 +0700

    d

--- re-learn-vue/vue3-cms/vite.config.js
+++ re-learn-vue/vue3-cms/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { viteMockServe } from 'vite-plugin-mock'
+// import { viteMockServe } from 'vite-plugin-mock'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
@@ -10,7 +10,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    viteMockServe({ supportTs: false }),
+    // viteMockServe({ supportTs: false }),
     AutoImport({
       resolvers: [AntDesignVueResolver()],
     }),

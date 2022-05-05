7e630885f1d81deee1dcd04939d9119e490729c8
dahe <Dengdaheng67@gmail.com>
2022-05-05 17:43:57 +0700

    添加echarts图表--

--- re-learn-vue/vue3-cms/src/views/dash-Mannage/analyze.vue
+++ re-learn-vue/vue3-cms/src/views/dash-Mannage/analyze.vue
@@ -1,3 +1,84 @@
 <template>
-    <div>分析页</div>
+  <div class="analyze-container">
+      <div class="static-container left-aprt">
+          <div id="myAnalyzeEcharts" :style="{ width: '100%', height: '300px' }"></div>
+      </div>
+  </div>
 </template>
+<script setup>
+import * as echarts from 'echarts'
+import { onMounted, onUnmounted } from 'vue'
+ /// 声明定义一下echart
+    let echart = echarts;
+    onMounted(() => {
+      initChart();
+    });
+
+    onUnmounted(() => {
+      echart.dispose;
+    });
+	
+    // 基础配置一下Echarts
+    const  initChart = () =>  {
+      let chart = echart.init(document.getElementById("myAnalyzeEcharts"), "dark");
+      // 把配置和数据放这里
+      chart.setOption({
+        xAxis: {
+          type: "category",
+          data: [
+            "一月",
+            "二月",
+            "三月",
+            "四月",
+            "五月",
+            "六月",
+            "七月",
+            "八月",
+            "九月",
+            "十月",
+            "十一月",
+            "十二月"
+          ]
+        },
+        tooltip: {
+          trigger: "axis"
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            data: [
+              820,
+              932,
+              901,
+              934,
+              1290,
+              1330,
+              1320,
+              801,
+              102,
+              230,
+              4321,
+              4129
+            ],
+            type: "line",
+            smooth: true
+          }
+        ]
+      });
+      window.onresize = function() {
+        //自适应大小
+        chart.resize();
+      };
+    }
+</script>
+<style lang="scss" scoped>
+.analyze-container {
+    display: flex;
+    flex-direction: row;
+    .static-container {
+        width: 100%;
+    }
+}
+</style>
--- re-learn-vue/vue3-cms/src/views/dash-Mannage/monitor.vue
+++ re-learn-vue/vue3-cms/src/views/dash-Mannage/monitor.vue
@@ -1,3 +1,77 @@
 <template>
-    <div>监控页</div>
+  <div class="monitor-container">
+      <div class="static-container left-aprt">
+          <div id="myMonitorEcharts" :style="{ width: '100%', height: '300px' }"></div>
+      </div>
+  </div>
 </template>
+<script setup>
+import * as echarts from 'echarts'
+import { onMounted, onUnmounted } from 'vue'
+ /// 声明定义一下echart
+    let echart = echarts;
+    onMounted(() => {
+      initChart();
+    });
+
+    onUnmounted(() => {
+      echart.dispose;
+    });
+	
+    // 基础配置一下Echarts
+    const  initChart = () =>  {
+      let chart = echart.init(document.getElementById("myMonitorEcharts"), "dark");
+      // 把配置和数据放这里
+      chart.setOption({
+  title: {
+    text: 'World Population'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  legend: {},
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: {
+    type: 'value',
+    boundaryGap: [0, 0.01]
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
+  },
+  series: [
+    {
+      name: '2011',
+      type: 'bar',
+      data: [18203, 23489, 29034, 104970, 131744, 630230]
+    },
+    {
+      name: '2012',
+      type: 'bar',
+      data: [19325, 23438, 31000, 121594, 134141, 681807]
+    }
+  ]
+});
+      window.onresize = function() {
+        //自适应大小
+        chart.resize();
+      };
+    }
+</script>
+<style lang="scss" scoped>
+.monitor-container {
+    display: flex;
+    flex-direction: row;
+    .static-container {
+        width: 100%;
+    }
+}
+</style>

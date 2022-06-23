5a9205a6d526c42669285d20c4d9c0127045e129
dahe <Dengdaheng67@gmail.com>
2022-06-23 15:12:06 +0700

    learn ---

---
+++ react18-learn/myapp/src/01-base/16-props.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavBar from './NavBar/index'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar></NavBar>
+            </div>
+        )
+    }
+}
+
+export default App;
--- react18-learn/myapp/src/index.js
+++ react18-learn/myapp/src/index.js
@@ -9,13 +9,20 @@ import ReactDOM from 'react-dom/client'
 // import App from  './01-base/07-ref'
 // import App from  './01-base/08-state'
 // import App from  './01-base/09-循环渲染'
-import App from  './01-base/10-todolist'
+// import App from  './01-base/10-todolist'
+// import App from  './01-base/12-卖座选项卡'
+// import App from  './01-base/13-setstate同步异步'
+// import App from  './01-base/14-betterScroll'
+import App from  './01-base/16-props'
 const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+// <React.StrictMode>
+//     <App/>
+// </React.StrictMode>
+// )
 root.render(
-<React.StrictMode>
-    <App/>
-</React.StrictMode>
-)
+        <App/>
+    )
 
 /**
  * jsx语法 js+xmls

64d4e0f0cf60295e40b064be5ab18a88e77e3d35
dahe <Dengdaheng67@gmail.com>
2022-09-09 14:39:54 +0700

    react继续学习--

---
+++ react18-learn/rl-v6-router/src/05-Redux/redux/actionCreator/TabbarActionCreator.js
@@ -0,0 +1,13 @@
+function show () {
+    return {
+        type: 'show-tabbar'
+    }
+}
+
+function hide () {
+    return {
+        type: 'hide-tabbar'
+    }
+}
+
+export { show, hide  }
--- react18-learn/rl-v6-router/src/App.js
+++ react18-learn/rl-v6-router/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter} from 'react-router-dom'
 import MRouter from './router/index'
 import TabBar from './components/TabBar'
+import store from './05-Redux/redux/store'
+import './views/css/App.css'
+
+// store.subscribe 订阅
 
 const App = () =>  {
+
+  const [isShow, setIsShow] = useState(store.getState().show)
+
+  useEffect(() => {
+    store.subscribe(() => {
+      console.log("app 中订阅", store.getState())
+      setIsShow(store.getState().show)
+    })
+  }, [])
+
   return (
     <BrowserRouter>
-      <MRouter/>
-      <TabBar/>
+      <MRouter></MRouter>
+      { isShow ? <TabBar/> : null }
     </BrowserRouter>
   )
 }

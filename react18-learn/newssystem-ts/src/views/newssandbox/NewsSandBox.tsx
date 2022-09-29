beaf9a0922744fe04d64fe3cc9d86f750f5a1192
dahe <Dengdaheng67@gmail.com>
2022-09-29 15:01:38 +0700

    项目构建，布局layout--

--- react18-learn/newssystem-ts/src/views/newssandbox/NewsSandBox.tsx
+++ react18-learn/newssystem-ts/src/views/newssandbox/NewsSandBox.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import SideMenu from '../../layout/SideMenu'
-import TopHeader from '../../layout/TopHeader'
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import SideMenu from "../../layout/SideMenu";
+import TopHeader from "../../layout/TopHeader";
+import "./NewsSandBox.css";
+
+const { Content } = Layout;
 
 export default function NewsSandBox() {
-    return (
-        <div>
-            <SideMenu></SideMenu>
-            <TopHeader></TopHeader>
-            <Outlet/>
-        </div>
-    )
+  return (
+    <Layout>
+      <SideMenu />
+      <Layout className="site-layout">
+        <TopHeader />
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }

d8cc491e07374da2729abeacfa98ffe15e25f463
dahe <Dengdaheng67@gmail.com>
2022-10-03 15:50:51 +0700

    构建左侧菜单栏--

--- react18-learn/newssystem-ts/src/views/right-mannage/RightList.tsx
+++ react18-learn/newssystem-ts/src/views/right-mannage/RightList.tsx
@@ -1,9 +1,39 @@
-import React, { memo } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table } from 'antd'
+import axios from 'axios'
+import { render } from '@testing-library/react';
 
 const RightList = () => {
+
+    const [dataSource, setDataSource] = useState([])
+    const columns = [
+        {
+          title: 'ID',
+          dataIndex: 'id',
+          key: 'id'
+        },
+        {
+          title: '权限名称',
+          dataIndex: 'label',
+          key: 'label',
+        },
+        {
+          title: '权限路径',
+          dataIndex: 'key',
+          key: 'key',
+        },
+      ];
+
+      useEffect(() => {
+          axios.get('http://localhost:3000/rights').then(res => {
+              console.log(res.data)
+              setDataSource(res.data)
+          })
+      },[])
+
     return (
         <div>
-            RightList
+            <Table dataSource={dataSource} columns={columns} />
         </div>
     )
 }

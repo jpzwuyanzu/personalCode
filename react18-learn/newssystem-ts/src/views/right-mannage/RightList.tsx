f1c8cef59bcd7a00625d0e77e14cffcac850aad8
dahe <Dengdaheng67@gmail.com>
2022-10-03 17:29:40 +0700

    a

--- react18-learn/newssystem-ts/src/views/right-mannage/RightList.tsx
+++ react18-learn/newssystem-ts/src/views/right-mannage/RightList.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Table } from 'antd'
+import { Table, Space, Button, Modal } from 'antd';
 import axios from 'axios'
+import {
+    EditOutlined,
+    DeleteOutlined,
+    ExclamationCircleOutlined
+} from "@ant-design/icons";
 import { render } from '@testing-library/react';
 
 const RightList = () => {
@@ -10,7 +15,10 @@ const RightList = () => {
         {
           title: 'ID',
           dataIndex: 'id',
-          key: 'id'
+          key: 'id',
+          render: (text: any) => {
+              return <b>{ text }</b>
+          }
         },
         {
           title: '权限名称',
@@ -22,18 +30,44 @@ const RightList = () => {
           dataIndex: 'key',
           key: 'key',
         },
+        {
+            title: '操作',
+            render: (text: any) => {
+                return <Space>
+                    <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                    <Button danger shape="circle" icon={<DeleteOutlined />} onClick={ () => showModal() } />
+                </Space>
+            }
+        }
       ];
 
-      useEffect(() => {
-          axios.get('http://localhost:3000/rights').then(res => {
-              console.log(res.data)
-              setDataSource(res.data)
-          })
-      },[])
+    useEffect(() => {
+        axios.get('http://localhost:3000/rights?_embed=children').then(res => {
+            console.log(res.data)
+            const list = res.data
+            list[0].children = '';
+            setDataSource(res.data)
+        })
+    },[])
+    const showModal = () => {
+        Modal.confirm({
+            title: 'Confirm',
+            icon: <ExclamationCircleOutlined />,
+            content: '确认删除该权限吗？',
+            okText: '确认',
+            cancelText: '取消',
+            onOk: () => {
+                confirmDelete()
+            }
+          });
+    }
+    const confirmDelete = () => {
+        console.log('确认删除')
+    }
 
     return (
         <div>
-            <Table dataSource={dataSource} columns={columns} pagination={{ pageSize: 3 }}/>
+            <Table dataSource={dataSource} columns={columns} pagination={{ pageSize: 10 }}/>
         </div>
     )
 }

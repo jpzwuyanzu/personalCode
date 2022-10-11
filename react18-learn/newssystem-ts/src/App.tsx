0142dc1d166a13e1665d8991c70a2d0e74602f38
dahe <Dengdaheng67@gmail.com>
2022-10-11 19:48:22 +0700

    修复警告信息--

--- react18-learn/newssystem-ts/src/App.tsx
+++ react18-learn/newssystem-ts/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
 import MRouter from './router/index'
-import { Spin } from 'antd'
 import './App.css'
 
 const App = () => {

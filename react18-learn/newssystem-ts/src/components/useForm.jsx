777e1140ecc42b1c9ed71c7711eafc8934647c59
dahe <Dengdaheng67@gmail.com>
2022-10-11 13:59:50 +0700

    构建用户列表模块--

---
+++ react18-learn/newssystem-ts/src/components/useForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Form } from 'antd'
+
+export default function useForm() {
+    return (
+        <Form
+            name="新增用户"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input />
+            </Form.Item>
+            </Form>
+    )
+}
--- react18-learn/newssystem-ts/src/views/user-mannage/UserList.tsx
+++ react18-learn/newssystem-ts/src/views/user-mannage/UserList.tsx
@@ -1,11 +1,146 @@
-import React, { memo } from 'react'
+import React, { useState, useEffect } from "react";
+import { Table, Space, Button, Modal, Form, Input, Select } from "antd";
+import axios from "axios";
+const { Option } = Select;
 
 const UserList = () => {
-    return (
-        <div>
-            UserList
-        </div>
-    )
-}
+  const [dataSource, setDataSource] = useState([]);
+  const [regions, setRegions] = useState([]);
+  const [roleList, setRoleList] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const columns: any = [
+    {
+      title: "用户名",
+      dataIndex: "username",
+      key: "username",
+      align: "center",
+    },
+    {
+      title: "地区",
+      dataIndex: "region",
+      key: "region",
+      align: "center",
+    },
+    {
+      title: "角色ID",
+      dataIndex: "roleId",
+      key: "roleId",
+      align: "center",
+    },
+    {
+      title: "操作",
+      align: "center",
+      render: () => {
+        return (
+          <>
+            <Space>
+              <Button type="primary">编辑</Button>
+              <Button type="primary" danger>
+                删除
+              </Button>
+            </Space>
+          </>
+        );
+      },
+    },
+  ];
 
-export default UserList
+  useEffect(() => {
+    axios.get("user.json").then((res) => {
+      setDataSource(res.data.users);
+    });
+  }, []);
+  useEffect(() => {
+    axios.get("regions.json").then((res) => {
+        setRegions(res.data.regions);
+    });
+  }, []);
+  useEffect(() => {
+    axios.get("role.json").then((res) => {
+        setRoleList(res.data.roles);
+    });
+  }, []);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        onClick={showModal}
+        style={{ marginBottom: "20px" }}>
+        新增用户
+      </Button>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        rowKey={(item: any) => item.id}/>
+      <Modal
+        title="新增用户"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}>
+        <Form
+          name="basic"
+          labelCol={{ span: 5 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off">
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[{ required: true, message: "请输入用户名称!" }]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: "请输入密码!" }]}>
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+          label="区域"
+          name="region"
+          rules={[{ required: true, message: "请选择用户区域!" }]}>
+            <Select>
+                {
+                    regions.map((item: any) => <Option value={ item.value } key={ item.id }>{ item.title }</Option>)
+                }
+            </Select>
+          </Form.Item>
+          <Form.Item
+          label="角色"
+          name="roleId"
+          rules={[{ required: true, message: "请选择用户角色!" }]}>
+             <Select>
+                {
+                    roleList.map((item: any) => <Option value={ item.id } key={ item.id }>{ item.roleName }</Option>)
+                }
+            </Select>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
+};
+
+export default UserList;

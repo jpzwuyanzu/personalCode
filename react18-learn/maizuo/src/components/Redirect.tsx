d43f829727b35ed0ceade2672353f9297d22327c
dahe <Dengdaheng67@gmail.com>
2022-10-18 17:33:29 +0700

    搭建react-typescript的H5项目

---
+++ react18-learn/maizuo/src/components/Redirect.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function Redirect({ to }: any) {
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate(to, { replace: true })
+    }, [])
+    return null;
+}

6eb0010f2f96eeca5aa78be49837fa9f79b65f96
XiMeng11 <peterliloveyou@gmail.com>
2023-05-04 14:03:36 +0700

    播放内页的组件封装成通过detail组件获取数据

--- re-learn-vue/yinghua-h5/src/views/detail/Detail.vue
+++ re-learn-vue/yinghua-h5/src/views/detail/Detail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="movie_detail_container">
         <MovieInfo :movieInfoData="movieInfoData"/>
-        <CusPlayer/>
+        <CusPlayer :playerInfo="playerInfo"/>
         <GuessLike/>
     </div>
 </template>
@@ -22,6 +22,236 @@ const movieInfoData = reactive<any>({
         desc: '《蚁人3》将会深入探索量子领域这个神秘的空间，有关量子领域在以前已经做出了很好的铺垫，比如说《蚁人》中出现的量子城市、一些奇怪的生物，以及《复联4》的时空隧道等等，引入反派征服者·康，这是一位来自于未...'
     }
 })
+const playerInfo = reactive<any>({
+        resourceList: [
+            {
+                name: '量子资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '天空资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '飞速资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: 'FOX资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '优质资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '无尽资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '新浪资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '百度资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '快车资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '卧龙资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: 'ikun资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '阳光资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '闪电资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '光速资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            },
+            {
+                name: '八戒资源',
+                collection: [
+                    {
+                        name: 1,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    },
+                    {
+                        name: 2,
+                        videoUrl: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+                        posterUrl: '//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg',
+                    }
+                ]
+            }
+        ]
+
+    })
 </script>
 <style lang="scss">
 .movie_detail_container {

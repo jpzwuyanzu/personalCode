156a636e29536dad1e7212dade4013e94f01c17b
XiMeng11 <peterliloveyou@gmail.com>
2023-05-12 10:41:18 +0700

    s

--- react18-learn/yinghua-admin/src/router/index.tsx
+++ react18-learn/yinghua-admin/src/router/index.tsx
@@ -1,4 +1,35 @@
-// import React, { Suspense } from 'react'
-// import { useRoutes } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import { useRoutes } from 'react-router-dom'
 
-// const Login = React.lazy(() => import())
+const Login: any = React.lazy(() => import('../views/login/Login'));
+const PageSandBox = React.lazy(() => import('../layout/page-sand-box/PageSandBox'));
+const AdList = React.lazy(() => import('../views/ad-mannage/AdList'));
+const Home = React.lazy(() => import('../views/home-mannage/Home'));
+const RoleList = React.lazy(() => import('../views/role-mannage/RoleList'));
+const UserList = React.lazy(() => import('../views/user-mannage/UserList'));
+
+const MRouter = () => {
+    const element = useRoutes([
+        {
+            path: '/login',
+            element: <Suspense><Login/></Suspense>
+        },
+        {
+            path: '/',
+            element: <Suspense><PageSandBox/></Suspense>,
+            children: [
+                {
+                    path: '',
+                    element: <Home/>
+                },
+                {
+                    path: 'home',
+                    element: <Suspense><Home/></Suspense>
+                },
+                {
+                    
+                }
+            ]
+        }
+    ])
+}

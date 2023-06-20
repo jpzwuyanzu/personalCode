9a8563fa2693e6c42df47db1265fd514cd1750ab
XiMeng11 <peterliloveyou@gmail.com>
2023-06-20 17:42:15 +0700

    s

---
+++ pwa-test/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script setup>
+import axios from 'vue-axios'
+//请修改成可用的测试API，对应vite.config.js
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(err => console.log('Request Failed', err)); 
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>

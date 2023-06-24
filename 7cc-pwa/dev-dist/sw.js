14af921c92de15617b4027049c18ba5d51ed777b
ximeng <peterliloveyou@gmail.com>
2023-06-24 11:15:24 +0700

    优化部分问题--

--- 7cc-pwa/dev-dist/sw.js
+++ 7cc-pwa/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b7fccfec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bhi5njp4pjo"
+    "revision": "0.sidgi82vv5o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

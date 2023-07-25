61d6289f859dfa15b2f5f4a0a87a225f64089b18
XiMeng11 <peterliloveyou@gmail.com>
2023-07-25 17:48:16 +0700

    s

--- react18-learn/yinghua-admin/src/api/index.ts
+++ react18-learn/yinghua-admin/src/api/index.ts
@@ -1,10 +1,10 @@
 import request from './../utils/request'
 
 export function getDemo (params: any) {
-    return request.get('/test/load', {params})
+    return request.get('/test/load', params)
 }
 export function postDemo (params: any) {
-    return request.post('/test/post', params)
+    return request.post('/test/post', {params})
 }
 /**
  * 支付后台接口
